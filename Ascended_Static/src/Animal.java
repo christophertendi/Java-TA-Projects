d0d582b42d0bf8dd9ee14f6af143259f33bcb897
Chris <chrissamuell77@gmail.com>
2021-05-20 16:22:05 +0700

    first time using terminal in git :>

---
+++ Ascended_Static/src/Animal.java
@@ -0,0 +1,7 @@
+public class Animal {
+
+    public void sleep() {
+        System.out.println("Sleep time...");
+    }
+
+}
---
+++ Ascended_Static/src/Main.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+
+        Animal a = new Animal();
+        Animal b = new Dog();
+
+        a.sleep();
+        b.sleep();
+
+    }
+}
